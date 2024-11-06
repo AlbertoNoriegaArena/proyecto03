7a48637f0aaaaf81a88621ffc040703de3e4f914
curso06 <curso06@gmail.com>
2024-11-06 11:18:40 +0100

    Test Calculadora

--- src/main/java/es/santander/ascender/proyecto03/App.java
+++ src/main/java/es/santander/ascender/proyecto03/App.java
@@ -6,7 +6,13 @@ package es.santander.ascender.proyecto03;
 public class App {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        long resultado = calculadora.sumar(4, 5);
-        System.out.println(resultado);
+        long resultadoSuma = calculadora.sumar(4, 5);
+        System.out.println(resultadoSuma);
+        long resultadoResta = calculadora.restar(10, 2);
+        System.out.println(resultadoResta);
+        long resultadoProducto = calculadora.multiplicar(4, 5);
+        System.out.println(resultadoProducto);
+        double resultadoDivision = calculadora.dividir(10, 2);
+        System.out.println(resultadoDivision);
     }
 }
---
+++ src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
@@ -0,0 +1,44 @@
+package es.santander.ascender.proyecto03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+
+    public void testSumar() {
+
+        Calculadora calculadora = new Calculadora();
+        long resultado = calculadora.sumar(10, 15);
+
+        assertEquals(25, resultado, "10+15 debería ser 25");
+    }
+
+    @Test
+    public void testRestar() {
+
+        Calculadora calculadora = new Calculadora();
+        long resultado = calculadora.restar(15, 10);
+
+        assertEquals(5, resultado, "15-10 debería ser 5");
+    }
+
+    @Test
+    public void testMultiplicar() {
+
+        Calculadora calculadora = new Calculadora();
+        long resultado = calculadora.multiplicar(2, 5);
+
+        assertEquals(10, resultado, "2 * 5 debería ser 10");
+    }
+
+    @Test
+    public void testDividir() {
+
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.dividir(10, 5);
+
+        assertEquals(2, resultado, "10 / 5 debería ser 2");
+    }
+}
